5bad7ebbf7c3a7d3d95601e7331e81e2eafa74dd
Sanjaana26 <sanjaana.suss@gmail.com>
2024-06-30 07:19:04 -0700

    modified

--- php/config.php
+++ php/config.php
@@ -1,5 +1,14 @@
 <?php 
+ $database_url = getenv('DATABASE_URL') ?: postgresql://userdetails_user:7i0TzF6FHVLDGSJoM4aASrMtnL1mtL8K@dpg-cq0ma6aju9rs73avjckg-a/userdetails
+ $parsed_url = parse_url($database_url);
  
- $con = mysqli_connect("localhost","root","","task") or die("Couldn't connect");
+ $host = $parsed_url['host'];
+ $dbname = substr($parsed_url['path'], 1);
+ $user = $parsed_url['user'];
+ $pass = $parsed_url['pass'];
+ 
+ $mysqli = new mysqli($host, $user, $pass, $dbname);
+ 
+//  $con = mysqli_connect("localhost","root","","task") or die("Couldn't connect");
 
 ?>
